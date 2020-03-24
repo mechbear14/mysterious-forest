47f930cdcc43445b70fbf03dae94f15284701b0d
Gabe Lyu <no6xichengavenue@gmail.com>
2020-03-24 04:08:04 +0000

    Added test cases

--- objects.py
+++ objects.py
@@ -11,15 +11,17 @@ class Forest:
 
     def go_to(self, x: int, y: int):
         if self.graph.check_accessible(self.position[0], self.position[1], x, y):
-            self.position = [x, y]
-            self.approachables = self.graph.get_direct_accessible(x, y)
+            self.update_position(x, y)
             print(f"Walked past. You're now at Cell({x}, {y})")
         elif (x, y) in self.approachables:
             # If successfullly opened the tile
             self.graph.make_accessible(
                 self.position[0], self.position[1], x, y)
-            self.position = [x, y]
-            self.approachables = self.graph.get_direct_accessible(x, y)
+            self.update_position(x, y)
             print(f"Opened. You're now at Cell({x}, {y})")
         else:
             print(f"You can't go to Cell({x}, {y})")
+
+    def update_position(self, x: int, y: int):
+        self.position = [x, y]
+        self.approachables = self.graph.get_direct_accessible(x, y)
